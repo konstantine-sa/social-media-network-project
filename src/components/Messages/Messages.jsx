347e389fcb6434563d4457b07a10506fe283b43a
konstantine-sa <konstantine.sa@gmail.com>
2023-06-22 21:02:29 +0200

    messages component basic

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,7 +1,23 @@
-import classNames from "./Messages.element.css";
+import classNames from "./Messages.module.css";
 
-const Dialogs = (props) => {
-  return <div>Messages</div>;
+const Messages = () => {
+  console.log(classNames);
+  return (
+    <div className={classNames.dialogs__wrapper}>
+      <div className={classNames.dialogs}>
+        <div className={classNames.dialog + " " + classNames.active}>
+          Person 1
+        </div>
+        <div className={classNames.dialog}>Person 2</div>
+        <div className={classNames.dialog}>Person 3</div>
+      </div>
+      <div className={classNames.messages}>
+        <div className={classNames.message}>Hello!</div>
+        <div className={classNames.message}>Whats up, buddy?</div>
+        <div className={classNames.message}>Please call me!</div>
+      </div>
+    </div>
+  );
 };
 
-export default Dialogs;
+export default Messages;
