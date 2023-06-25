d15cf2d0967962eb93e48d4b61c88834f470441b
konstantine-sa <konstantine.sa@gmail.com>
2023-06-25 18:19:45 +0200

    Profile component renamed to MyPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Messages from "./components/Messages/Messages";
 import Header from "./components/Header/Header";
 import Nav from "./components/Nav/Nav";
-import Profile from "./components/Profile/Profile";
+import Profile from "./components/MyPage/Profile";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
