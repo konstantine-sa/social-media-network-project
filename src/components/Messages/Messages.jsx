7719c5754e234c3f8c33d5310c5d8bb60e753800
konstantine-sa <konstantine.sa@gmail.com>
2023-06-25 19:43:24 +0200

    .map method added to Messages

--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -3,6 +3,8 @@ import classNames from "./Messages.module.css";
 
 const setActive = ({ isActive }) => (isActive ? classNames.active : "");
 
+//===================================== DIALOG ==============================================
+
 const Dialog = (props) => {
   return (
     <div className={classNames.dialog}>
@@ -13,26 +15,43 @@ const Dialog = (props) => {
   );
 };
 
+let dialogsData = [
+  { id: 1, name: "Lina" },
+  { id: 2, name: "Marcus" },
+  { id: 3, name: "Lucas" },
+  { id: 4, name: "Andrew" },
+  { id: 5, name: "Andrew" },
+];
+
+let dialogsElements = dialogsData.map((dial) => (
+  <Dialog name={dial.name} id={dial.id} key={dial.id} />
+));
+
+//===================================== MESSAGES ==============================================
+
 const Message = (props) => {
   return <div className={classNames.message}>{props.message}</div>;
 };
 
+let messagesData = [
+  { id: 1, message: "Hello, man!" },
+  { id: 2, message: "Whats up, buddy?" },
+  { id: 3, message: "Why dont you respond?" },
+  { id: 4, message: "Please call me!" },
+  { id: 5, message: "Please call me!!!!!!!!!!!" },
+];
+
+let messageElements = messagesData.map((mess) => {
+  return <Message message={mess.message} key={mess.id} />;
+});
+
+//===================================== end of MESSAGES ==============================================
+
 const Messages = () => {
-  console.log(classNames);
   return (
     <div className={classNames.dialogs__wrapper}>
-      <div className={classNames.dialogs}>
-        <Dialog name="Person 123" id="1" />
-        <Dialog name="Person 223" id="2" />
-        <Dialog name="Person 334" id="3" />
-        <Dialog name="Person 445" id="4" />
-      </div>
-      <div className={classNames.messages}>
-        <Message message="Hello, man!" />
-        <Message message="Whats up, buddy?" />
-        <Message message="Why dont you respond?" />
-        <Message message="Please call me!" />
-      </div>
+      <div className={classNames.dialogs}>{dialogsElements}</div>
+      <div className={classNames.messages}>{messageElements}</div>
     </div>
   );
 };
