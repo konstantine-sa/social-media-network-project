d742a84309487f8934d1a6e907d766020f3542ee
konstantine-sa <konstantine.sa@gmail.com>
2023-07-02 17:33:36 +0200

    Context refactored

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,16 @@ import ReactDOM from "react-dom/client";
 import store from "./redux/redux-store";
 import App from "./App.jsx";
 import "./index.css";
-import StoreContext from "./StoreContext";
+import { Provider } from "./StoreContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-console.log(StoreContext);
 
 let renderEntireTree = (state) => {
   root.render(
     <React.StrictMode>
-      <StoreContext.Provider value={store}>
+      <Provider store={store}>
         <App />
-      </StoreContext.Provider>
+      </Provider>
     </React.StrictMode>
   );
 };
